1c8e3a3742132de4a1bd9a2d138697d97ddf8a17
Richard Peterson <peterson4487@live.missouristate.edu>
2023-05-04 22:15:33 -0500

    Giving up for now.

--- LaserBolt.gd
+++ LaserBolt.gd
@@ -5,11 +5,15 @@ var damage = 20
 var velocity = Vector2.ZERO
 var parent
 
-func _ready():
-	var direction = parent.global_transform.x
-	velocity = direction * (speed + parent.velocity.length())
-	rotation = direction.angle()
+#func _ready():
+#	var direction = parent.global_transform.x
+#	velocity = direction * (speed + parent.velocity.length())
+#	rotation = direction.angle()
+#
+#
+#func _physics_process(delta):
+#	position += velocity * delta
 
 
 func _physics_process(delta):
-	position += velocity * delta
+	global_position += Vector2(0, -1).rotated(rotation) * speed * delta
--- LaserCannon.gd
+++ LaserCannon.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
-export (PackedScene) var laserBolt
+#export (PackedScene) var laserBolt
+var laserBoltScene = preload("res://LaserBolt.tscn")
 
 
 func fire():
@@ -9,7 +10,13 @@ func fire():
 
 
 func spawnLaser():
-	var instance = laserBolt.instance()
-	var sceneRoot = get_tree().root.get_children()[0]
-	instance.parent = get_parent()
-	sceneRoot.add_child(instance)
+#	var instance = laserBolt.instance()
+#	var sceneRoot = get_tree().root.get_children()[0]
+#	instance.parent = get_parent()
+#	sceneRoot.add_child(instance)
+
+
+	var instance = laserBoltScene.instance()
+	instance.global_position = global_position
+	instance.rotation = rotation
+	add_child(instance)
